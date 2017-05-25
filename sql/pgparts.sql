308f0fa5c6ed6d320789ab76a73a1881210ac0ca
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-05-25 16:36:49 +0100

    Added functions archive_before(), unarchive_partition()

--- sql/pgparts.sql
+++ sql/pgparts.sql
@@ -351,6 +351,21 @@ $$
         and contype = 'p');
 $$;
 
+create function
+_children("table" regclass) returns setof regclass
+language sql stable as
+$$
+    select inhrelid::regclass from pg_inherits
+    where inhparent = "table";
+$$;
+
+create function
+_parents("table" regclass) returns setof regclass
+language sql stable as
+$$
+    select inhparent::regclass from pg_inherits
+    where inhrelid = "table";
+$$;
 
 create function
 _is_range(type regtype) returns boolean
@@ -434,7 +449,7 @@ $$;
 
 
 create type partition_state as
-    enum ('unpartitioned', 'missing', 'present', 'detached');
+    enum ('unpartitioned', 'missing', 'present', 'detached', 'archived');
 
 create type partition_info as (
     state @extschema@.partition_state,
@@ -456,22 +471,42 @@ begin
     end if;
 
     rv.partition = @extschema@.partition_for("table", value);
-    if rv.partition is null then
-        rv.state = 'missing';
-        return rv;
-    end if;
-
-    perform 1 from pg_inherits
-    where inhparent = "table" and inhrelid = rv.partition;
-    if found then
-        rv.state = 'present';
-    else
-        rv.state = 'detached';
-    end if;
-
+    rv.state = @extschema@._partition_state(rv.partition);
     return rv;
 end
 $$;
+
+
+create function
+_partition_state(part regclass) returns text
+language plpgsql stable as
+$$
+declare
+    parent regclass;
+begin
+    if part is null then
+        return 'missing';
+    end if;
+
+    select base_table
+    from @extschema@.existing_partition p
+    where p.partition = part
+    into parent;
+    if not found then
+        return null;
+    end if;
+
+    if part in (select @extschema@._children(parent)) then
+        return 'present';
+    elsif part in (select @extschema@._children(
+            @extschema@._archive_table(parent))) then
+        return 'archived';
+    else
+        return 'detached';
+    end if;
+end
+$$;
+
 
 create function
 _partitions("table" regclass) returns setof regclass
@@ -479,8 +514,8 @@ language sql as
 $$
     select p.partition
     from @extschema@.existing_partition p
-    join pg_inherits i on p.partition = inhrelid
-    where p.base_table = "table" and inhparent = "table";
+    where p.base_table = "table"
+    and p.partition in (select @extschema@._children("table"));
 $$;
 
 
@@ -1085,8 +1120,7 @@ begin
     if partition in (select @extschema@._partitions("table")) then
         raise notice '%', format('detaching partition %s from %s',
             partition, "table");
-        execute format('alter table %s no inherit %s',
-            partition, "table");
+        perform @extschema@._no_inherit(partition, "table");
         perform @extschema@.maintain_insert_function("table");
     end if;
 
@@ -1110,12 +1144,35 @@ begin
     if partition not in (select @extschema@._partitions("table")) then
         raise notice '%', format('attaching partition %s to %s',
             partition, "table");
-        execute format('alter table %s inherit %s',
-            partition, "table");
+        perform @extschema@._inherit(partition, "table");
         perform @extschema@.maintain_insert_function("table");
     end if;
 
     return partition;
+end
+$$;
+
+
+create function
+_inherit(child regclass, parent regclass) returns void
+language plpgsql as
+$$
+begin
+    if child is null then raise 'child cannot be null'; end if;
+    if parent is null then raise 'parent cannot be null'; end if;
+    execute format('alter table %s inherit %s', child, parent);
+end
+$$;
+
+
+create function
+_no_inherit(child regclass, parent regclass) returns void
+language plpgsql as
+$$
+begin
+    if child is null then raise 'child cannot be null'; end if;
+    if parent is null then raise 'parent cannot be null'; end if;
+    execute format('alter table %s no inherit %s', child, parent);
 end
 $$;
 
@@ -1260,6 +1317,67 @@ begin
     end if;
 
     return rv;
+end
+$$;
+
+
+create function
+archive_before("table" regclass, ts timestamptz) returns setof regclass
+language plpgsql as
+$$
+declare
+    archive regclass = @extschema@._archive_table("table");
+    part regclass;
+begin
+    if archive is null then
+        raise using
+            message = format('archive table for %s not found', "table"),
+            hint = format(
+                'You should run "@extschema@.create_archive(%s)" before.',
+                "table");
+    end if;
+
+    for part in
+        select p.partition from @extschema@.existing_partition p
+        where p.partition in (select @extschema@._partitions("table"))
+        and p.end_value::timestamptz < ts
+        order by p.start_value::timestamptz
+    loop
+        raise notice 'archiving partition %', part;
+        perform @extschema@._no_inherit(part, "table");
+        perform @extschema@._inherit(part, archive);
+        return next part;
+    end loop;
+
+    perform @extschema@.maintain_insert_function("table");
+end
+$$;
+
+
+create function
+unarchive_partition(part regclass) returns regclass
+language plpgsql as
+$$
+declare
+    state partition_state = @extschema@._partition_state(part);
+    archive regclass;
+    parent regclass;
+begin
+    if state != 'archived' then
+        raise 'The table % is not an archived partition', part;
+    end if;
+
+    select @extschema@._parents(part) into strict archive;
+    select base_table from @extschema@.existing_partition p
+        where p.partition = part
+        into strict parent;
+
+    raise notice 'unarchiving partition %', part;
+    perform @extschema@._no_inherit(part, archive);
+    perform @extschema@._inherit(part, parent);
+
+    perform @extschema@.maintain_insert_function(parent);
+    return part;
 end
 $$;
 
--- test/sql/archive.sql
+++ test/sql/archive.sql
@@ -1,22 +1,64 @@
 \pset format unaligned
 \pset footer off
 \set VERBOSITY terse
-create schema partest;
+create schema arctest;
 set datestyle = 'ISO';
 
-create extension pgparts with schema partest;
+create extension pgparts with schema arctest;
 
 create table at (
     id serial primary key,
     day date not null,
     data text);
 
-select partest.setup('at', 'day', 'monthly');
+-- Create some partitions and data
+select arctest.setup('at', 'day', 'monthly');
+select arctest.create_for('at', '2017-01-10');
+select arctest.create_for('at', '2017-02-10');
+select arctest.create_for('at', '2017-03-10');
+insert into at (day) values ('2017-01-10');
+insert into at (day) values ('2017-02-10');
 
-select partest.create_archive('at');
-select count(*) from at_all;
-select count(*) from at_archived;
+-- Must create the archive before
+select arctest.archive_before('at', '2017-02-15');
 
-select partest.create_archive('at');
-select count(*) from at_all;
-select count(*) from at_archived;
+select arctest.create_archive('at');
+select * from at order by id;
+select * from at_all order by id;
+select * from at_archived order by id;
+
+-- Idempotent
+select arctest.create_archive('at');
+select * from at order by id;
+select * from at_all order by id;
+select * from at_archived order by id;
+
+-- Archiving partitions
+select arctest.archive_before('at', '2017-02-15');
+select * from arctest.info('at', '2017-01-15');
+select * from arctest.info('at', '2017-02-15');
+select * from at order by id;
+select * from at_all order by id;
+select * from at_archived order by id;
+
+-- Archived partition can't receive data
+insert into at (day) values ('2017-01-10');
+insert into at (day) values ('2017-02-10');
+select * from at order by id;
+select * from at_all order by id;
+select * from at_archived order by id;
+
+-- Archived partition can be unarchived
+select arctest.unarchive_partition('at_201701');
+select arctest.unarchive_partition('at_201702');
+select * from arctest.info('at', '2017-01-15');
+select * from arctest.info('at', '2017-02-15');
+
+-- Unarchived partition can receive data
+insert into at (day) values ('2017-01-10');
+insert into at (day) values ('2017-02-10');
+select * from at order by id;
+select * from at_all order by id;
+select * from at_archived order by id;
+
+drop extension pgparts;
